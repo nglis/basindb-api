f95937e26958dfc28884322d0ab3ae67ad4217a3
nglis <nglisic10@gmail.com>
2021-11-16 21:51:30 -0700

    well_loading

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ axios.get('https://basin.gdr.nrcan.gc.ca/wells/index_e.php')
         // Get all available areas
         $('select[name=area] option', html).each(function() {
             const area = $(this).text();
-            areas.push(area)
+            areas.push(area.split(' ').join('%20')) // Changes spaces to URL space characters
         })
     }).catch(err => console.log(err))
 
@@ -58,35 +58,47 @@ app.get('/info', (req, res) => {
     }
 })
 
-app.get('/wells', (req, res) => {
+app.get('/wells', async (req, res) => {
     try {
         const path = documentPaths.filter(document => document.id === 'wells')[0].path
-        const URL = basinBaseURL + path
+        // const URL = basinBaseURL + path
+        let baseURL = 'https://basin.gdr.nrcan.gc.ca/wells/query.php?level=1&areas='
 
-        axios.get(URL)
+        // build the url using available areas
+        baseURL += areas.join('_')
+
+        const wells = []
+
+        await axios.get(baseURL)
             .then(response => {
                 const html = response.data
-                const $ = cheerio.load(html)
+                // Doing some string magic here
+                const data = html.split('WELL(S) RETURNED **\");')[1]
+                // res.json(data.split(";"))
+                data.split(";").forEach(d => {
+                    try {
+                        const trimStart = d.split("myNewOption(")[1]
+                        const trimEnd = trimStart.split(",")[0]
+                        const removeBackslash = trimEnd.split('\\').join('')
+                        const removeDblQuotes = removeBackslash.split('"').join('')
 
-                const areas = []
-                const wells = []
-
-                // Get all available areas
-                const a = $('select[name=area] option', html).each(function() {
-                    const area = $(this).text();
-                    areas.push(area)
+                        wells.push(removeDblQuotes)
+                    } catch (err) {console.log(err)}
                 })
-
-                // Get all available wells
-                const b = $("select[name='wellid_select[]'] option", html) // Gets all available well options
-
-                console.log(a.length)
-                console.log(b.length)
-                // each(function() {
-                //     console.log($(this).text())
-                // })
-                res.json({html})
             }).catch(err => console.log(err))
+
+            console.log(wells)
+
+        let wellURL = 'https://basin.gdr.nrcan.gc.ca/wells/well_query_e.php?well=' + wells[0]
+        // wells.forEach(well => 
+        //     wellURL += 'well=' + well + '&')
+        //     console.log(wellURL)
+            
+            // console.log(wellURL)
+        await axios.get(wellURL)
+            .then(response => {
+                res.json(response.data)
+            })
 
     } catch (err) {
         console.log(err)
